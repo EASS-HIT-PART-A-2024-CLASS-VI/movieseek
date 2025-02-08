ec1060a205e132681f3a5e304974414d9348ea30
Vladislav Rabski <vrabski@gmail.com>
2025-02-08 20:22:30 +0200

    Added functionality, Now you can login to a page named Home

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Main = () => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        flexDirection: "column",
+      }}
+    >
+      <h2>Welcome to the Movie App</h2>
+      <p>This is where all future functionalities will be added.</p>
+    </div>
+  );
+};
+
+export default Main;
